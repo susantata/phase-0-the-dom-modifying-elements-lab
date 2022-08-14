bfaa2e11adc0971f02a3d75e31b4961872ce5cd7
+ <+>
2022-08-14 17:05:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main').remove();
+document.createElement("h1");
+const newHeader= document.createElement("h1");
+newHeader.id =`victory`;
+newHeader.innerHTML= "YOUR-NAME is the champion" ;
